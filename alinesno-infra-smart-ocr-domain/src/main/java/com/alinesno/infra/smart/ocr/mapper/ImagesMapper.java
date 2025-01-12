371bf66cf0e480325f95be8c069b1f3c8f06b447
李伟(Michael) <michaelAip@gmail.com>
2025-01-13 07:59:59 +0800

    feat:在ImagesMapper中导入IBaseMapper接口

--- alinesno-infra-smart-ocr-domain/src/main/java/com/alinesno/infra/smart/ocr/mapper/ImagesMapper.java
+++ alinesno-infra-smart-ocr-domain/src/main/java/com/alinesno/infra/smart/ocr/mapper/ImagesMapper.java
@@ -1,7 +1,7 @@
 package com.alinesno.infra.smart.ocr.mapper;
 
-import com.alinesno.infra.smart.ocr.entity.ImagesEntity;
 import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.ocr.entity.ImagesEntity;
 
 /**
  * <p>

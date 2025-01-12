d9d7f2b2c116671cad8f0e8c3fe60d4125d6fbd6
李伟(Michael) <michaelAip@gmail.com>
2025-01-13 07:59:59 +0800

    refactor: 移除 ProjectController 类中未使用的 Result 导入

--- alinesno-infra-smart-ocr-gateway/src/main/java/com/alinesno/infra/smart/ocr/api/controller/ProjectController.java
+++ alinesno-infra-smart-ocr-gateway/src/main/java/com/alinesno/infra/smart/ocr/api/controller/ProjectController.java
@@ -16,7 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.xnio.Result;
 
 
 /**

2dfe6b3ce6339c0686180d232b5e59c1e51e2a6b
李伟(Michael) <michaelAip@gmail.com>
2025-01-13 07:59:59 +0800

    feat:在ModelServiceImpl中添加Spring Service注解

--- alinesno-infra-smart-ocr-domain/src/main/java/com/alinesno/infra/smart/ocr/service/impl/ModelServiceImpl.java
+++ alinesno-infra-smart-ocr-domain/src/main/java/com/alinesno/infra/smart/ocr/service/impl/ModelServiceImpl.java
@@ -1,11 +1,10 @@
 package com.alinesno.infra.smart.ocr.service.impl;
 
-import com.alinesno.infra.smart.ocr.mapper.ModelMapper;
-import org.springframework.stereotype.Service;
-
-import com.alinesno.infra.smart.ocr.entity.ModelEntity;
-import com.alinesno.infra.smart.ocr.service.IModelService;
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.smart.ocr.entity.ModelEntity;
+import com.alinesno.infra.smart.ocr.mapper.ModelMapper;
+import com.alinesno.infra.smart.ocr.service.IModelService;
+import org.springframework.stereotype.Service;
 
 /**
  * <p>

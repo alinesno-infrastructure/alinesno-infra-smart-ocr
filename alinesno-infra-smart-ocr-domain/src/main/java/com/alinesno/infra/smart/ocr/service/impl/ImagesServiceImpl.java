49aac1daf366690764aa0d4c7fae460f01513315
李伟(Michael) <michaelAip@gmail.com>
2025-01-13 07:59:59 +0800

    refactor: 对 ImagesServiceImpl 类进行重构，优化导入语句和类结构

--- alinesno-infra-smart-ocr-domain/src/main/java/com/alinesno/infra/smart/ocr/service/impl/ImagesServiceImpl.java
+++ alinesno-infra-smart-ocr-domain/src/main/java/com/alinesno/infra/smart/ocr/service/impl/ImagesServiceImpl.java
@@ -1,13 +1,12 @@
 package com.alinesno.infra.smart.ocr.service.impl;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
-
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
 import com.alinesno.infra.smart.ocr.entity.ImagesEntity;
 import com.alinesno.infra.smart.ocr.mapper.ImagesMapper;
 import com.alinesno.infra.smart.ocr.service.IImagesService;
-import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 /**
  * <p>

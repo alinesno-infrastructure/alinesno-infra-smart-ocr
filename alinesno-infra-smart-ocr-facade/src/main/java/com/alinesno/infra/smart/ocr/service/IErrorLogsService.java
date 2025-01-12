7fc8d8a06f22e77f59b2ee5d435007d6750516bc
李伟(Michael) <michaelAip@gmail.com>
2025-01-13 07:59:59 +0800

    feat:在IErrorLogsService中引入错误日志实体类

--- alinesno-infra-smart-ocr-facade/src/main/java/com/alinesno/infra/smart/ocr/service/IErrorLogsService.java
+++ alinesno-infra-smart-ocr-facade/src/main/java/com/alinesno/infra/smart/ocr/service/IErrorLogsService.java
@@ -1,7 +1,7 @@
 package com.alinesno.infra.smart.ocr.service;
 
-import com.alinesno.infra.smart.ocr.entity.ErrorLogsEntity;
 import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.ocr.entity.ErrorLogsEntity;
 
 /**
  * <p>

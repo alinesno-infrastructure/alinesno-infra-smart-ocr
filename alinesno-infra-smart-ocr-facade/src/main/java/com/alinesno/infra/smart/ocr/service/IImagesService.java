6c7be07ffc5398cb559c0cad737716a1bdda16b0
李伟(Michael) <michaelAip@gmail.com>
2025-01-13 07:59:59 +0800

    feat:在IImagesService中引入IBaseService接口

--- alinesno-infra-smart-ocr-facade/src/main/java/com/alinesno/infra/smart/ocr/service/IImagesService.java
+++ alinesno-infra-smart-ocr-facade/src/main/java/com/alinesno/infra/smart/ocr/service/IImagesService.java
@@ -1,7 +1,7 @@
 package com.alinesno.infra.smart.ocr.service;
 
-import com.alinesno.infra.smart.ocr.entity.ImagesEntity;
 import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.ocr.entity.ImagesEntity;
 
 /**
  * <p>

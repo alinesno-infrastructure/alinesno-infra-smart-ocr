c57fa50b64f66dc20ab2b568c3e9d3d8919741ae
李伟(Michael) <michaelAip@gmail.com>
2025-01-13 07:59:59 +0800

    feat:新增 AppConfiguration 类，配置数据范围、流程管理、Mapper扫描及日志切面

---
+++ alinesno-infra-smart-ocr-boot/src/main/java/com/alinesno/infra/smart/ocr/config/AppConfiguration.java
@@ -0,0 +1,30 @@
+package com.alinesno.infra.smart.ocr.config;
+
+import com.alinesno.infra.common.extend.datasource.enable.EnableInfraDataScope;
+import com.alinesno.infra.common.facade.enable.EnableActable;
+import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
+import com.alinesno.infra.common.web.log.aspect.LogAspect;
+import com.dtflys.forest.springboot.annotation.ForestScan;
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@EnableInfraDataScope
+@EnableActable
+@MapperScan("com.alinesno.infra.smart.ocr.mapper")
+@EnableInfraSsoApi
+@ForestScan({
+        "com.alinesno.infra.common.web.adapter.base.consumer" ,
+        "com.alinesno.infra.base.platform.adapter"
+})
+@Configuration
+public class AppConfiguration {
+
+    @Bean
+    public LogAspect getLogAspect(){
+        return new LogAspect() ;
+    }
+
+}

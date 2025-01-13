1279c1d88204b84005b00362c66868ebd4921ed6
李伟(Michael) <michaelAip@gmail.com>
2025-01-13 08:00:00 +0800

    refactor: 精简 SmartOcrApplication 类，移除不必要的注解和方法

--- alinesno-infra-smart-ocr-boot/src/main/java/com/alinesno/infra/smart/ocr/SmartOcrApplication.java
+++ alinesno-infra-smart-ocr-boot/src/main/java/com/alinesno/infra/smart/ocr/SmartOcrApplication.java
@@ -1,31 +1,18 @@
 package com.alinesno.infra.smart.ocr;
 
-import com.alinesno.infra.common.facade.enable.EnableActable;
-import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
-import com.alinesno.infra.common.web.log.aspect.LogAspect;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 /**
  * 集成一个Java开发示例工具
- * @author LuoAnDong
- * @since 2023年8月3日 上午6:23:43
+ * @author luoxiaodong
+ * @version 1.0.0
  */
-@EnableActable
-@MapperScan("com.alinesno.infra.smart.ocr.mapper")
-@EnableInfraSsoApi
 @SpringBootApplication
 public class SmartOcrApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartOcrApplication.class, args);
-	}
-
-	@Bean
-	public LogAspect getLogAspect(){
-		return new LogAspect() ;
 	}
 
 }

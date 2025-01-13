aa47715924cd2ae22a69cd1412cd1beb6299b21b
李伟(Michael) <michaelAip@gmail.com>
2025-01-13 08:00:00 +0800

    feat:添加OcrResultsMapper接口实现

--- alinesno-infra-smart-ocr-domain/src/main/java/com/alinesno/infra/smart/ocr/mapper/OcrResultsMapper.java
+++ alinesno-infra-smart-ocr-domain/src/main/java/com/alinesno/infra/smart/ocr/mapper/OcrResultsMapper.java
@@ -1,7 +1,7 @@
 package com.alinesno.infra.smart.ocr.mapper;
 
-import com.alinesno.infra.smart.ocr.entity.OcrResultsEntity;
 import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.ocr.entity.OcrResultsEntity;
 
 /**
  * <p>

7cd0d1f7ad67d0dadd16494cba82a0c70f04489d
王磊(Levi) <leviAip@gmail.com>
2025-01-24 00:10:07 +0800

    feat:在OcrRequestDto类中添加图片文件路径属性

--- alinesno-infra-smart-ocr-facade/src/main/java/com/alinesno/infra/smart/ocr/api/OcrRequestDto.java
+++ alinesno-infra-smart-ocr-facade/src/main/java/com/alinesno/infra/smart/ocr/api/OcrRequestDto.java
@@ -2,11 +2,14 @@ package com.alinesno.infra.smart.ocr.api;
 
 import lombok.Data;
 
+import java.io.File;
+
 /**
  * OCR请求基类，用于封装所有OCR识别请求的共有字段。
  */
 @Data
 public class OcrRequestDto {
+    private File filePath ; // 图片文件路径
     private String imageBase64; // 图片的Base64编码字符串
     private String recognitionType; // 识别类型标识符
 }

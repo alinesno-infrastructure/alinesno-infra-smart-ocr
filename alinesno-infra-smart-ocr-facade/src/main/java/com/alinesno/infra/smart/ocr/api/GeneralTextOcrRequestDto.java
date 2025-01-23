aca9cb7f85d90b39aaa5bacccb24a0551f2beee1
王磊(Levi) <leviAip@gmail.com>
2025-01-24 00:10:07 +0800

    feat:在 `GeneralTextOcrRequestDto` 类中添加 `@EqualsAndHashCode` 注解

--- alinesno-infra-smart-ocr-facade/src/main/java/com/alinesno/infra/smart/ocr/api/GeneralTextOcrRequestDto.java
+++ alinesno-infra-smart-ocr-facade/src/main/java/com/alinesno/infra/smart/ocr/api/GeneralTextOcrRequestDto.java
@@ -1,9 +1,11 @@
 package com.alinesno.infra.smart.ocr.api;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * 通用文字识别请求DTO
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class GeneralTextOcrRequestDto extends OcrRequestDto {}
